6a3b0b6999092ee53acbd7c10af97a81eb63c90c
Nazar <nazar20050716@gmail.com>
2023-03-15 19:03:07 +0100

    Task01 fixed comments

--- src/com/example/ppj04/Task01.java
+++ src/com/example/ppj04/Task01.java
@@ -2,6 +2,10 @@ package com.example.ppj04;
 
 public class Task01 {
     public static void main(String[] args) {
+        printMethod();
+    }
+
+    private static void printMethod() {
         System.out.println("Task01");
     }
 }

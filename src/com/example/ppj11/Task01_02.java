c140532b943e338591692fc2b6d0239495076dd6
Nazar <nazar20050716@gmail.com>
2023-04-11 17:04:13 +0200

    PPj11 fixed comment

--- src/com/example/ppj11/Task01_02.java
+++ src/com/example/ppj11/Task01_02.java
@@ -5,26 +5,26 @@ import java.util.Arrays;
 public class Task01_02 {
     public static void main(String[] args) {
         int[] array = {23, 56, 12, 40, 80, 9};
-        int max = array[0];
-        int min = array[0];
+        int maxIndex = array[0];
+        int minIndex = array[0];
         for (int i = 0; i < array.length; i++) {
-            if (array[i] > max) {
-                max = array[i];
+            if (array[i] > maxIndex) {
+                maxIndex = array[i];
             }
-            if (array[i] < min) {
-                min = array[i];
+            if (array[i] < minIndex) {
+                minIndex = array[i];
             }
         }
         System.out.println("TASK 1");
-        System.out.println(max + " " + min);
-        System.out.println(max - min);
+        System.out.println(maxIndex + " " + minIndex);
+        System.out.println(maxIndex - minIndex);
         System.out.println("TASK 2");
         System.out.println(Arrays.toString(array));
         for (int i = 0; i < array.length; i++) {
-            if (array[i] == min) {
-                array[i] = max;
-            } else if (array[i] == max) {
-                array[i] = min;
+            if (array[i] == minIndex) {
+                array[i] = maxIndex;
+            } else if (array[i] == maxIndex) {
+                array[i] = minIndex;
             }
         }
         System.out.println(Arrays.toString(array));

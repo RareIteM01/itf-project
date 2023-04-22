4e7552d6c7f1f9f73c04d836f52456aef966c0b0
Nazar <nazar20050716@gmail.com>
2023-04-22 11:45:05 +0200

    ppj09 task 3 fixed comment

--- src/com/example/ppj09/Task03.java
+++ src/com/example/ppj09/Task03.java
@@ -4,16 +4,14 @@ import java.util.Scanner;
 
 public class Task03 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("Enter a byte value between 0 and 15: ");
-        byte byteValue = input.nextByte();
-
-        char hexValue = getHexValue(byteValue);
-        System.out.println("The hexadecimal value is: " + hexValue);
+        for (byte i = 0; i <= 15; i++) {
+            System.out.println(getHexValue(i));
+        }
     }
+
     public static char getHexValue(byte byteValue) {
-        char[] hexDigits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-        return hexDigits[byteValue];
+        String str = Integer.toHexString(byteValue);
+        System.out.println(str);
+        return str.charAt(0);
     }
 }
